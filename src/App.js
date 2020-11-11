4db571aebf54eeeaaf02c661de192b6f5b00df62
Juliano Costa <apfjuliano@gmail.com>
2020-11-11 00:48:25 -0300

    App data - Lesson 05

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react'
 import './styles/app.scss'
 import Player from './components/Player'
 import Song from './components/Song'
+import data from './data'
 
 function App() {
+    document.title = 'Music Player'
     return (
         <div className="App">
             <Song />
